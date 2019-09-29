8c57a4fa2fb34b58236b4a36b214855ec6c55b91
Thais Costa <thethaiscosta@gmail.com>
2019-09-29 00:39:47 -0300

    Adiciona implementação para salvar os dados.

--- MinhasAnotacoesAula/MinhasAnotacoesAula/ViewController.swift
+++ MinhasAnotacoesAula/MinhasAnotacoesAula/ViewController.swift
@@ -12,18 +12,50 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textoTextView: UITextView!
     
+    let minhaAnotacaoChave = "MinhaAnotacao"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //recuperar dados
+        textoTextView.text = recuperadrDado()
     }
+    
     //serve para capturar o toque na tela!
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true) 
+       esconderTeclado()
     }
     
     @IBAction func salvarAnotacao(_ sender: Any) {
-        
+        esconderTeclado()
+        if let texto = textoTextView.text{
+            SalvarDadosAnotacao(texto: texto)
+            mostrarAlerta(texto)
+        }
     }
     
+    func SalvarDadosAnotacao(texto: String) {
+        UserDefaults.standard.set(texto, forKey: minhaAnotacaoChave)
+    }
+    
+    func recuperadrDado() -> String? {
+        let textorecuperado = UserDefaults.standard.object(forKey: minhaAnotacaoChave)
+        return textorecuperado as? String
+    }
+    
+    func mostrarAlerta(_ texto: String) {
+        let alerta = UIAlertController(title: "Anotação", message: "Anotação salva com sucesso! 😉 \n\n\(texto)", preferredStyle: .alert)
+        let fechar = UIAlertAction(title: "Ok", style: .default)
+        alerta.addAction(fechar)
+        present(alerta, animated: true)
+    }
+    
+    func esconderTeclado() {
+        view.endEditing(true)
+    }
+    
+    // Só funciona sem a navegation! Serve para esconder o statusBar
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
-
