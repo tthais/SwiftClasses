1cac47ee0f635dcc1689e2004f4b32c2ab369237
Thais Costa <thethaiscosta@gmail.com>
2019-11-17 23:35:31 -0300

    Adiciona negar e ativar localização no App.

--- PokemonGo/PokemonGo/ViewController.swift
+++ PokemonGo/PokemonGo/ViewController.swift
@@ -22,4 +22,22 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     gerenciadorLocalizacao.requestWhenInUseAuthorization()
     gerenciadorLocalizacao.startUpdatingLocation()
   }
+  //Negando autorização de Localização
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+      let alertcontroller = UIAlertController(title: "Permisão de localização", message: "Gostaria de ativar sua localização para caçar Pokemons?", preferredStyle: .alert)
+
+      let acaoConfiguracoes = UIAlertAction(title: "Ajustes", style: .default) { _ in
+        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+      }
+
+      let acaoCancelar = UIAlertAction(title: "Cancelar", style: .destructive)
+
+      alertcontroller.addAction(acaoConfiguracoes)
+      alertcontroller.addAction(acaoCancelar)
+
+      present(alertcontroller, animated: true)
+    }
+  }
 }
