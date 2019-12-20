c91792778d97ece383c897a78ca15ef12c0c7ec8
Thais Costa <thethaiscosta@gmail.com>
2019-12-20 00:30:23 -0300

    Add Navegação Poké Agenda

---
+++ PokemonGo/PokemonGo/PokeAgendaViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PokeAgendaViewController.swift
+//  PokemonGo
+//
+//  Created by Thais Costa on 19/12/19.
+//  Copyright © 2019 tthais. All rights reserved.
+//
+
+import UIKit
+
+class PokeAgendaViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func voltarMapa(_ sender: Any) {
+    dismiss(animated: true)
+  }
+}
